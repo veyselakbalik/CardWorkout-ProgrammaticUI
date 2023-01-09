02ed7369a3b7dcb1baf96d4469333d011d670c38
veyselakbalik <veyselakbalik@ogr.iu.edu.tr>
2023-01-10 00:21:31 +0300

    Add ImageView to CardSelectionVC

--- CardWorkout-ProgrammaticUI/CardSelectionVC.swift
+++ CardWorkout-ProgrammaticUI/CardSelectionVC.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class CardSelectionVC: UIViewController {
+    
+    let cardImageView   = UIImageView()
+    let stopButton      = CWButton(backgroundColor: .systemRed, title: "Stop!")
+    let resetButton     = CWButton(backgroundColor: .systemGreen, title: "Reset")
+    let rulesButton     = CWButton(backgroundColor: .systemBlue, title: "Rules")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .brown
+        view.backgroundColor = .systemBackground
+        configureUI()
     }
+    
+    func configureUI() {
+        configureCardImageView()
+    }
+    
+    func configureCardImageView() {
+        view.addSubview(cardImageView)
+        cardImageView.translatesAutoresizingMaskIntoConstraints = false
+        cardImageView.image = UIImage(named: "AS")
+        
+        NSLayoutConstraint.activate([
+            cardImageView.widthAnchor.constraint(equalToConstant: 250),
+            cardImageView.heightAnchor.constraint(equalToConstant: 350),
+            cardImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            cardImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -75)
+            
+        ])
+    }
+    
     
 
    
