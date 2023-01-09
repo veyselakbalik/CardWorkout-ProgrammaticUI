1d67d2ca1fdb9d27eec2fff991f38624ae2346b3
veyselakbalik <akbalikveyc@gmail.com>
2023-01-09 23:58:33 +0300

    Create button for ui

---
+++ CardWorkout-ProgrammaticUI/CWButton.swift
@@ -0,0 +1,38 @@
+//
+//  CWButton.swift
+//  CardWorkout-ProgrammaticUI
+//
+//  Created by Veysel Akbalık on 8.01.2023.
+//
+
+import UIKit
+
+class CWButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+        
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor : UIColor, title : String){
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        setTitle(title, for: .normal)
+        
+        
+    }
+    
+    func configure() {
+        layer.cornerRadius = 8
+        titleLabel?.font = .systemFont(ofSize: 19, weight: .bold)
+        setTitleColor(.white, for: .normal)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
---
+++ CardWorkout-ProgrammaticUI/CardSelectionVC.swift
@@ -0,0 +1,21 @@
+//
+//  CardSelectionVC.swift
+//  CardWorkout-ProgrammaticUI
+//
+//  Created by Veysel Akbalık on 6.01.2023.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .brown
+    }
+    
+
+   
+
+}
